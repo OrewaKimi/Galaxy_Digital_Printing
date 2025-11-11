ed624bacb3375649f8932b2287d6c608144b166c
OrewaKimi <kimi93.com@gmail.com>
2025-11-11 14:21:46 +0700

    add package bootsrap

---
+++ bootstrap/providers.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    App\Providers\AppServiceProvider::class,
+    App\Providers\Filament\AdminPanelProvider::class,
+    App\Providers\Filament\CustomerPanelProvider::class,
+    App\Providers\Filament\CustomerServicePanelProvider::class,
+    App\Providers\Filament\DesignerPanelProvider::class,
+    App\Providers\Filament\ProductionPanelProvider::class,
+];
