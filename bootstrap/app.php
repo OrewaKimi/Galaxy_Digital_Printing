e7cffa72314a969032696cbe76091c57a91addc5
OrewaKimi <kimi93.com@gmail.com>
2025-12-20 21:56:18 +0700

    Fix: Add webhook/midtrans to CSRF exceptions

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->validateCsrfTokens(except: [
             'payment/midtrans/callback',
             'midtrans/callback',
+            'webhook/midtrans',
         ]);
         
         $middleware->alias([
